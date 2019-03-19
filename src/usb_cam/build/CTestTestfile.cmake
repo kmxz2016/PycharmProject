d8208a025f0c13187ae855d559bb518c6c6308e5
MIN <601985329@qq.com>
2019-03-19 16:08:28 +0800

    v2.0

--- src/usb_cam/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/km/catkin_ws/usb_cam/src
-# Build directory: /home/km/catkin_ws/usb_cam/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(usb_cam)
--- src/usb_cam/build/usb_cam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/km/catkin_ws/usb_cam/src/usb_cam
-# Build directory: /home/km/catkin_ws/usb_cam/build/usb_cam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
